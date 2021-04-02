915d5118f390658f8f7f5323eb1dbd294c4bd390
Andy Han <36119601+andyqhan@users.noreply.github.com>
2021-04-02 12:13:00 -0400

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
 ln -s ./kitty ~/.config/kitty
-ln -s ./doom.d ~/.doom.d
-ln -s ./zshenv ~/.zshenv
-ln -s ./zshrc ~/.zshrc
-ln -s ./skhdrc ~/.skhdrc
-ln -s ./yabairc ~/.yabairc
+ln -s ./.doom.d ~/.doom.d
+ln -s ./.zshenv ~/.zshenv
+ln -s ./.zshrc ~/.zshrc
+ln -s ./.skhdrc ~/.skhdrc
+ln -s ./.yabairc ~/.yabairc
 
 
